813a57ea65c2e1c2187b5af147e2fe2737735686
Sergey Chebotar <s.chebotar@gmail.com>
2022-02-17 18:52:36 +0300

    Add ShapesUtil

---
+++ Mindbox.Shapes/ShapesUtil.cs
@@ -0,0 +1,11 @@
+﻿namespace Mindbox.Shapes
+{
+    public static class ShapesUtil
+    {
+        public static double GetTriangleArea(double a, double b, double c) =>
+            new Triangle(a, b, c).Area;
+
+        public static double GetCircleArea(double r) =>
+            new Circle(r).Area;
+    }
+}
